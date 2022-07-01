335adeacf9b60029e8a5c4f645d3514ff6c11994
Sarah <ss.guiheux@orange.fr>
2022-07-01 09:58:47 +0200

    version 1

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>A propos</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, facere quibusdam eos molestiae fugiat aperiam similique nam, cumque, dolorum et quia veniam? Perferendis, accusamus animi culpa ullam assumenda quam distinctio.</p>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam molestiae, provident officiis porro reiciendis facilis nihil, animi, ullam voluptatum atque earum nam exercitationem delectus autem quam similique expedita minus quia soluta ipsa id. Illum assumenda iusto similique nostrum id. Debitis quod, explicabo amet tenetur perspiciatis odio obcaecati quia accusantium nam atque fugit quos sequi ab quam officiis laborum, aperiam repellat ipsam impedit, voluptatibus aspernatur eveniet voluptate eum cupiditate! Deleniti incidunt nulla magnam neque recusandae ipsum totam provident impedit illum hic earum corrupti autem perspiciatis, ab quam, laudantium consequatur porro nobis nam perferendis atque enim minima? Aliquid assumenda exercitationem non pariatur minima dicta. Explicabo, repellat nulla non totam dicta at in blanditiis placeat rem animi accusamus? Modi repellat deserunt laudantium doloribus sed dolorem quidem deleniti dignissimos fugit distinctio ducimus commodi ipsam minus veniam, ratione excepturi blanditiis voluptatibus. Doloribus, atque quas laboriosam quidem explicabo, veniam minima odit amet cumque corporis consequatur soluta repellat odio ad quod dolores vitae possimus unde officiis nulla est, dicta expedita! Et atque nam cupiditate, sed inventore officia sit nisi accusamus fugiat iste mollitia voluptatum voluptatem, facere, corporis id quam amet. Ex unde consectetur, repellat accusantium laudantium cum ea quae dignissimos? Reiciendis assumenda inventore quibusdam quis, obcaecati similique.</p>
+        </div>
+    );
+};
+
+export default About;
